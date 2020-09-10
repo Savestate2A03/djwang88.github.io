2ba58d030c0737579009938cf800a83a707bc2fe
Savestate2a03 <joseph.elkhouri+github@gmail.com>
2020-09-09 20:59:35 -0400

    inconsistent whitespace

--- randomizer.js
+++ randomizer.js
@@ -502,30 +502,30 @@ function decodeRandomizerId(id) {
  */
 const base62 = {
 	charset: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-	  .split(''),
+		.split(''),
 	encode: integer => {
-	  if (integer === 0) {
-		return 0;
-	  }
-	  let s = [];
-	  while (integer > 0) {
-		s = [base62.charset[integer % 62], ...s];
-		integer = Math.floor(integer / 62);
-	  }
-	  return s.join('');
+		if (integer === 0) {
+			return 0;
+		}
+		let s = [];
+		while (integer > 0) {
+			s = [base62.charset[integer % 62], ...s];
+			integer = Math.floor(integer / 62);
+		}
+		return s.join('');
 	},
 	decode: chars => chars.split('').reverse().reduce((prev, curr, i) =>
-	  prev + (base62.charset.indexOf(curr) * (62 ** i)), 0)
-  };  
+		prev + (base62.charset.indexOf(curr) * (62 ** i)), 0)
+	};
 
 /*
  * includeJs by Svitlana Maksymchuk
  */
 function includeJs(jsFilePath) {
-    var js = document.createElement("script");
-    js.type = "text/javascript";
-    js.src = jsFilePath;
-    document.body.appendChild(js);
+	var js = document.createElement("script");
+	js.type = "text/javascript";
+	js.src = jsFilePath;
+	document.body.appendChild(js);
 }
 
 function loadCode() {
